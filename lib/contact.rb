3c35da6398be54e71598dac78b76d58cac404959
Joshua Atteberry <joshuaatteberry@gmail.com>
2015-01-15 19:08:40 -0800

    Add .all_contacts method and spec

--- lib/contact.rb
+++ lib/contact.rb
@@ -1,10 +1,14 @@
 class Contact
   attr_reader :contact_name, :contact_phone_number
+  @@all_contacts = []
 
   define_method(:initialize) do |attributes|
     @contact_name = attributes.fetch(:contact_name)
     @contact_phone_number = attributes.fetch(:contact_phone_number)
   end
 
+  define_singleton_method(:all_contacts) do
+    @@all_contacts
+  end
 
 end
--- spec/contact_spec.rb
+++ spec/contact_spec.rb
@@ -3,9 +3,21 @@ require('contact')
 
 describe(Contact) do
   describe('#initialize') do
-    it("takes the name and phone number for a newly created contact and returns that contact") do
+    it("takes the name and phone number for a newly created contact and returns contact name") do
       test_contact = Contact.new({:contact_name => "Joshua", :contact_phone_number => "5125676637"})
       expect(test_contact.contact_name()).to(eq("Joshua"))
     end
+    it("takes the name and phone number for a newly created contact and returns contact name") do
+      test_contact = Contact.new({:contact_name => "Joshua", :contact_phone_number => "5125676637"})
+      expect(test_contact.contact_phone_number()).to(eq("5125676637"))
+    end
   end
+
+  describe('.all_contacts') do
+    it("returns empty") do
+      test_contact = Contact.new({:contact_name => nil, :contact_phone_number => nil})
+      expect(Contact.all_contacts()).to(eq([]))
+    end
+  end
+
 end
